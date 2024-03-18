e1fa831ee3dd6e2409192b14ff213cb9e847bd47
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-19 05:23:44 +0530

    overlay adjusted and background image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Border from "./components/Border";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Border />
-    </div>
+    <Home>
+      <div className="flex justify-center items-center h-screen">
+        <Border />
+      </div>
+    </Home>
   );
 }
 
--- src/components/Border.jsx
+++ src/components/Border.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
 const Border = () => {
+  const handleCallIconClick = () => {
+    // Add functionality to display contact details
+    alert("Display contact details for call icon clicked!");
+  };
   return (
-    <div className="iphone14-home-screen h-screen flex flex-col items-center justify-center">
-      <img
-        src="./public/home-iphone.png"
-        alt="iPhone 14 Home Screen"
-        className="w-full h-full object-cover"
-      />
+    <div className="iphone14-home-screen relative">
+      {/* Container for the image and overlay */}
+      <div className="relative">
+        {/* Background image */}
+        <img
+          src="../public/home-iphone.png" // Update to use the PNG image
+          alt="iPhone 14 Home Screen"
+          className="w-350 h-713 object-cover"
+        />
+
+        {/* Overlay div */}
+        <div className="overlay absolute top-0 left-0 w-full h-full opacity-50 bg-black border rounded">
+          {/* Clickable area for the call icon */}
+          <div
+            className="call-icon clickable-area"
+            onClick={handleCallIconClick}
+            style={{
+              position: "absolute",
+              top: "150px", // Adjust top position according to the icon's position on the image
+              left: "80px", // Adjust left position according to the icon's position on the image
+              width: "50px", // Adjust width according to the icon's size on the image
+              height: "50px", // Adjust height according to the icon's size on the image
+            }}
+          ></div>
+          {/* Add more clickable areas for other icons as needed */}
+        </div>
+      </div>
     </div>
   );
 };
